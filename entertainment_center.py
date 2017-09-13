625becf53734c51d596d6217a202a9d430db3eef
Ankit Gupta <ankit6791@gmail.com>
2017-09-13 05:23:03 -0700

    Added all file. First commit

---
+++ entertainment_center.py
@@ -0,0 +1,29 @@
+import media, fresh_tomatoes
+
+toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come to life", 
+	"https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+	"https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+despicable_me = media.Movie("Despicable Me", "A conman with minions wants to set his mark", 
+	"https://upload.wikimedia.org/wikipedia/en/d/db/Despicable_Me_Poster.jpg",
+	"https://www.youtube.com/watch?v=sUkZFetWYY0")
+
+shutter_island = media.Movie("Shutter Island", "A thriller like no other", 
+	"https://upload.wikimedia.org/wikipedia/en/7/76/Shutterislandposter.jpg",
+	"https://www.youtube.com/watch?v=5iaYLCiq5RM")
+
+twelve_angry_men = media.Movie("12 Angry Men", "Life is in their hands, death is on their minds", 
+	"https://upload.wikimedia.org/wikipedia/en/9/91/12_angry_men.jpg",
+	"https://www.youtube.com/watch?v=A7CBKT0PWFA")
+
+the_prestige = media.Movie("The Prestige", "Welcome to the magical world of magic!", 
+	"https://upload.wikimedia.org/wikipedia/en/d/d2/Prestige_poster.jpg",
+	"https://www.youtube.com/watch?v=o4gHCmTQDVI")
+
+the_big_short = media.Movie("The Big Short", "Explains the 2008 housing market collapse", 
+	"https://upload.wikimedia.org/wikipedia/en/2/2f/Big-short-inside-the-doomsday-machine.jpg",
+	"https://www.youtube.com/watch?v=vgqG3ITMv1Q")
+
+movies=[toy_story, despicable_me, shutter_island, twelve_angry_men, the_prestige, the_big_short]
+
+fresh_tomatoes.open_movies_page(movies) 	
---
+++ media.py
@@ -0,0 +1,16 @@
+#Google Style Guide for Python suggests capital first letter for class name
+#The triple quotes are the values of a predefined class attribute __doc__ which can be used to describe a class
+
+import webbrowser
+class Movie():
+	""" This class provides a way to store information related to movies """
+	VALID_RATINGS = ["G", "PG", "PG-13", "R"]
+	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+		self.title = movie_title
+		self.storyline = movie_storyline
+		self.poster_image_url = poster_image
+		self.trailer_youtube_url = trailer_youtube
+
+	def show_trailer(self):
+		webbrowser.open(self.trailer_youtube_url)
+		
